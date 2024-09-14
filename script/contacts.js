c3ebb625129dcef7b7ff17db6df2db4da5b239ec
C1205 <93650306+C1205@users.noreply.github.com>
2024-09-14 07:40:28 +0000

    14 sept

--- script/contacts.js
+++ script/contacts.js
@@ -1,3 +1,46 @@
 let validate = () => {
-	// Fill in the necessary validation here
+	let name = document.getElementById("name");
+	let errorMsg = document.getElementById("nameError")
+	if (name.value.length != 0) {
+		name.classList = "form-control";
+		errorMsg.classList = "d-none text-danger";
+	}
+	else{
+		name.classList = "form-control is-invalid";
+		errorMsg.classList = "text-danger";
+		console.log("dang,u suck")
+	}
+	let email = document.getElementById("email");
+	let emailErrorMsg = document.getElementById("emailError")
+	if (email.value.length != 0) {
+		email.classList = "form-control";
+		emailErrorMsg.classList = "d-none text-danger";
+	}
+	else{
+		email.classList = "form-control is-invalid";
+		emailErrorMsg.classList = "text-danger";
+		console.log("what da hyell")
+	}
+	let subject = document.getElementById("subject");
+	let subjectErrorMsg = document.getElementById("subjectError")
+	if (subject.value.length != 0) {
+		subject.classList = "form-control";
+		subjectErrorMsg.classList = "d-none text-danger";
+	}
+	else{
+		subject.classList = "form-control is-invalid";
+		subjectErrorMsg.classList = "text-danger";
+		console.log("FAILURE LMAO")
+	}
+	let message = document.getElementById("message");
+	let messageErrorMsg = document.getElementById("MsgError")
+	if (message.value.length != 0) {
+		message.classList = "form-control";
+		messageErrorMsg.classList = "d-none text-danger";
+	}
+	else{
+		message.classList = "form-control is-invalid";
+		messageErrorMsg.classList = "text-danger";
+		console.log("BEIJING CORRRN")
+	}
 }
