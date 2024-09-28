99b0efa6fa4d641e76268ba951d8dfc746e618df
C1205 <93650306+C1205@users.noreply.github.com>
2024-09-28 07:48:30 +0000

    28 Sept

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,32 @@
-// 
+// javascript your world :O */
+const canvasBoard = document.getElementById("canvas1");
+let ctx = canvasBoard.getContext("2d");
+//blueberry pi 
+let X = 0
+let Y = 0
+let DX = 20;
+let DY = 20;
+let RADIUS = 20;
+
+let drawBall = () => {
+  if(document.getElementById("playAnimation").checked){
+        ctx.reset()
+        ctx.beginPath();
+        ctx.arc(X,Y,RADIUS,0,2*Math.PI);
+        ctx.fill();
+        ctx.closePath();
+
+        if(X < 0 || X > canvasBoard.width){
+            DX = -DX;
+        }
+        if(Y < 0 || Y > canvasBoard.height){
+            DY = -DY;
+        }
+        X += DX;
+        Y += DY;
+  }
+  requestAnimationFrame(drawBall);
+};
+
+
+drawBall();
